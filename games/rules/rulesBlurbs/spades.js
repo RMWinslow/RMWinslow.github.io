ab1c18920f0711f55fb8e0f717d1b921eb896469
RMWinslow <robertmartinwinslowks@gmail.com>
2019-12-28 04:12:48 -0600

    Card Game Rules
    What I did:
    -Created a new directory with rules for several card games.
    -Created a hub page for trick taking game and microgames.
    -Isolated the rules for specific games within .js files so they can be
    included in multiple hub pages:
    ——Hearts
    ——Spades
    ——Oh Hell!
    ——Win Lose Banana
    ——Devils Poker
    -Started making a hub page for Poker games.
    
    Left unfinished:
    -Example trick in Tricktaking Hub
    -Poker Hub:
    —Finish hand ranking
    —Maybe switch examples to svg images.

---
+++ games/rules/rulesBlurbs/spades.js
@@ -0,0 +1,31 @@
+document.write(`
+<h4 id="spades">Spades</h4>
+<i>Brother's favorite card game.</i>
+<p>(4) Team-based <a href="tricktaking.html">Trick-Taking</a>. Standard Deck, no Jokers.</p>
+<ul>
+    <li>2 teams of 2. Each person sits across from their teammate.</li>
+    <li>Spades are always the trump suit.</li>
+    <li>Each hand, the entire deck is dealt out. 13 cards to each player.</li>
+    <li>Each player bids how many tricks they think they will take. Teammate's bids are combined.</li>
+    <li>Within a hand, spades cannot be played as lead suit until at least one spade has already been played.</li>
+    <li>End-of-hand scoring:</li>
+        <ul>
+        <li>If team's bid is met or exceeded: +10×[bid]</li>
+        <li>If team's bid is not met: -10×[bid]</li>
+        <li>For each trick taken in excess of team bid: +1 point and +1 <b>bag</b>:</li>
+            <ul>
+            <li>Bags accumulate across hands.</li>
+            <li>Every 6th bag results in -100 points.</li>
+            </ul>
+        <li>Special case: If a player bids 0</li>
+            <ul>
+                <li>Any tricks that player earns are counted as bags instead of contributing to their teams total.</li>
+                <li>If that player takes exactly 0 tricks, their team earns +100 points</li>
+            </ul>
+        <li>Special Case: <b>Shooting the Moon.</b> If a team bids 13 and succeeds, they earn +200 points instead of +130</li>
+        </ul>
+    <li>First team to 300 points wins.</li>
+</ul>
+`)
+
+
---
+++ games/rules/rulesBlurbs/winLoseBanana.js
@@ -0,0 +1,17 @@
+document.write(`
+<h4>Win, Lose, Banana</h4>
+<i>Only fun because of how bad my spouse is at this.</i>
+<p>(3) Social Deduction. Three cards: King, Queen, Joker.</p>
+<ul>
+    <li>Deal each player a card.</li>
+    <li>The King reveals their card. The Queen and Joker keeps theirs secret.</li>
+    <li>Both the Queen and Joker try to convince the King that they are the true Queen.</li>
+    <li>The King chooses who they believe to be the Queen.</li>
+        <ul>
+            <li>If the King chooses correctly, both King and Queen win.</li>
+            <li>If the King chooses incorrectly, the Joker wins.</li>
+        </ul>
+</ul>
+`)
+
+
